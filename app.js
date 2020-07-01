d6c348db7489ffd9c1506792937f988ac4dfd085
azkanurf <azkanurfadillah@gmial.com>
2020-07-01 21:37:35 +0700

    format date

--- app.js
+++ app.js
@@ -19,13 +19,23 @@ connectDB();
 
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 //Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handlebars helper
+const { formatDate } = require("./helpers/hbs");
+
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({ helpers: { formatDate }, defaultLayout: "main", extname: ".hbs" })
+);
 app.set("view engine", ".hbs");
 
 //session
@@ -48,6 +58,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 5000;
 
